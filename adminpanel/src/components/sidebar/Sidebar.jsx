b392db97d5943fc29287374c1e226ac8df2489c9
thanadon <thanadon06452@gmail.com>
2025-04-26 15:42:20 +0700

    adminpanel 1.2 fixed

--- adminpanel/src/components/sidebar/Sidebar.jsx
+++ adminpanel/src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Sidebar = () => {
   return (
@@ -6,4 +6,4 @@ const Sidebar = () => {
   )
 }
 
-export default Sidebar
+export default Sidebar;
