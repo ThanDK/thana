6412faf1d3438c9583e35fe69504ad324ab059f8
thanadon <thanadon06452@gmail.com>
2025-05-09 12:27:35 +0700

    Adding Header 3.26.01

--- foodies/src/App.jsx
+++ foodies/src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Menubar from './components/Menubar/Menubar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Contact from './pages/Contact/Contact';
+import ExploreFood from './pages/ExploreFood/ExploreFood';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div>
-      <Menubar/>
+      <Menubar />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/explore" element={<ExploreFood />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
 export default App;
