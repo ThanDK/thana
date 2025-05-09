31b874043d095618f01964cb02025815659e5143
thanadon <thanadon06452@gmail.com>
2025-05-09 12:32:52 +0700

    Prepare for Category 3.28.29

--- foodies/src/App.jsx
+++ foodies/src/App.jsx
@@ -4,13 +4,13 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Contact from './pages/Contact/Contact';
 import ExploreFood from './pages/ExploreFood/ExploreFood';
-import Header from './components/Header/Header';
+
 
 const App = () => {
   return (
     <div>
       <Menubar />
-      <Header />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
