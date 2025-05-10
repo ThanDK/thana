0eaa06d0d1f98ad8bc089d37396429ecf98606c3
thanadon <thanadon06452@gmail.com>
2025-05-10 13:25:20 +0700

    Adding StoreContext UnknowTime

---
+++ foodies/src/context/StoreContext.jsx
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+
+export const StoreContext = createContext(null);
+
+export const StoreContextProvider = (props) => {
+
+    const contextValue = {
+
+    };
+
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
