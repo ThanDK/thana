51195806f9b7ea78a7fdb0639f68a353c0c90975
thanadon <thanadon06452@gmail.com>
2025-05-11 19:57:24 +0700

    FetchApi object StoreContext 4.00.0

--- foodies/src/context/StoreContext.jsx
+++ foodies/src/context/StoreContext.jsx
@@ -1,13 +1,32 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import axios from 'axios';
 
 export const StoreContext = createContext(null);
 
 export const StoreContextProvider = (props) => {
 
-    const contextValue = {
+    const [foodList , setFoodList] = useState([]);
 
+    const fetchFoodList = async () => {
+        try {
+            const response = await axios.get('http://localhost:8080/api/foods');
+            setFoodList(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error("Failed to fetch food list:", error);
+        }
+    }
+
+    const contextValue = {
+        foodList
     };
 
+    useEffect(() => {
+        async function loadData() {
+            await fetchFoodList();
+        }
+        loadData();
+    }, [])
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
