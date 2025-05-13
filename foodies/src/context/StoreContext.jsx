28cd46a5b472668e407adfdbdf2f683f4bd8d536
thanadon <thanadon06452@gmail.com>
2025-05-13 15:46:54 +0700

    adding FoodDetails 4.21.00

--- foodies/src/context/StoreContext.jsx
+++ foodies/src/context/StoreContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import axios from 'axios';
+import { fetchFoodList } from "../service/foodService";
 
 export const StoreContext = createContext(null);
 
@@ -7,26 +7,20 @@ export const StoreContextProvider = (props) => {
 
     const [foodList , setFoodList] = useState([]);
 
-    const fetchFoodList = async () => {
-        try {
-            const response = await axios.get('http://localhost:8080/api/foods');
-            setFoodList(response.data);
-            console.log(response.data);
-        } catch (error) {
-            console.error("Failed to fetch food list:", error);
-        }
-    }
-
     const contextValue = {
         foodList
     };
 
     useEffect(() => {
         async function loadData() {
-            await fetchFoodList();
+            const data = await fetchFoodList();
+            setFoodList(data)
+            
+            
         }
         loadData();
     }, [])
+
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
