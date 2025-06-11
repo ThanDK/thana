a9de7a30325a03d66550ab60e4d2955f28e99585
thanadon <thanadon06452@gmail.com>
2025-06-11 18:31:57 +0700

    Preparing For Service payment 12.30

---
+++ foodiesapi/src/main/java/in/thanadon/foodiesapi/config/PaypalConfig.java
@@ -0,0 +1,46 @@
+package in.thanadon.foodiesapi.config;
+
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.OAuthTokenCredential;
+import com.paypal.base.rest.PayPalRESTException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class PaypalConfig {
+
+    @Value("${paypal.client.id}")
+    private String clientId;
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
+    @Value("${paypal.mode}")
+    private String mode;
+
+    @Bean
+    public Map<String, String> payPalSDKConfig() {
+        Map<String, String> config = new HashMap<String, String>();
+        config.put("mode", mode);
+        return config;
+    }
+
+    @Bean
+    public OAuthTokenCredential oAuthTokenCredential() {
+        return new OAuthTokenCredential(clientId, clientSecret, payPalSDKConfig());
+    }
+
+    @Bean
+    public APIContext apiContext() {
+        APIContext context = null;
+        try {
+            context = new APIContext(oAuthTokenCredential().getAccessToken());
+        } catch (PayPalRESTException e) {
+            throw new RuntimeException(e);
+        }
+        context.setConfigurationMap(payPalSDKConfig());
+        return context;
+    }
+}
